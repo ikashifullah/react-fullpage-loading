cf06559eef834c45569c7733f0b0d5098b92af88
Kashif Ullah <kashifullahwebdeveloper@gmail.com>
2019-07-30 12:48:54 +0500

    Add component files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class FullPageLoading extends React.Component {
+    render () {
+        return (
+            <div>React Full Page Loading Module</div>
+        )
+    }
+}
