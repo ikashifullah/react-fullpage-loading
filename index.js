71391300440ca96d5ecaa350f2df970a09195915
Kashif Ullah <kashifullahwebdeveloper@gmail.com>
2019-07-31 10:10:19 +0500

    Add latest changes for v1.0.2

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _loading = _interopRequireDefault(require("./img/loading.gif"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -27,21 +31,63 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
+var styles = {
+  position: 'fixed',
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  background: 'rgba(255, 255, 255, 0.8)',
+  top: '0px',
+  left: '0px',
+  width: '100%',
+  height: '100%',
+  zIndex: '2147483647',
+  opacity: '1'
+};
+
 var FullPageLoading =
 /*#__PURE__*/
 function (_React$Component) {
   _inherits(FullPageLoading, _React$Component);
 
-  function FullPageLoading() {
+  function FullPageLoading(props) {
+    var _this;
+
     _classCallCheck(this, FullPageLoading);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(FullPageLoading).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(FullPageLoading).call(this, props));
+    _this.state = {
+      loading: true,
+      newStyle: null
+    };
+    return _this;
   }
 
   _createClass(FullPageLoading, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.setState({
+        newStyle: styles
+      });
+      var timer = this.props.timer ? this.props.timer : 2000;
+      setTimeout(function () {
+        _this2.setState({
+          newStyle: Object.assign({}, _this2.state.newStyle, _defineProperty({}, 'display', 'none'))
+        });
+      }, timer);
+    }
+  }, {
     key: "render",
     value: function render() {
-      return _react["default"].createElement("div", null, "React Full Page Loading Module");
+      var newStyle = this.state.newStyle;
+      return _react["default"].createElement("div", {
+        style: newStyle
+      }, _react["default"].createElement("img", {
+        src: _loading["default"]
+      }));
     }
   }]);
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import loadingImg from './img/loading.gif';
+
+let styles = {
+    position: 'fixed',
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    background: 'rgba(255, 255, 255, 0.8)',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    height: '100%',
+    zIndex: '2147483647',
+    opacity: '1',
+}
 
 export default class FullPageLoading extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+            newStyle: null
+		}
+	}
+
+	componentDidMount() {
+		this.setState({ newStyle: styles});
+        const timer =  this.props.timer ? this.props.timer : 2000;
+        setTimeout(() => {
+            this.setState({
+                newStyle: Object.assign(
+                    {}, 
+                    this.state.newStyle,
+                    { ['display']: 'none' }
+                  )
+            })
+        }, timer);
+
+	}
+
     render () {
+        const { newStyle} = this.state;
         return (
-            <div>React Full Page Loading Module</div>
+        	<div style={newStyle}>
+            	<img src={loadingImg} />
+            </div>
         )
     }
 }
